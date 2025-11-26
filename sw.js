976f280e59e76912073021ecb209c63f211e66c3
jonhvasquezbeltre-dev <jonhvasquezbeltre@gmail.com>
2025-11-26 00:05:06 -0400

    sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'eduquest-v1';
+const OFFLINE_URL = '/index.html';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/script.js',
+  '/icon.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      }).catch(() => caches.match(OFFLINE_URL));
+    })
+  );
+});
